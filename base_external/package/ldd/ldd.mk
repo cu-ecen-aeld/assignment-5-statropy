e523477362d36aabe612151acd737ae2b9fe4abe
Erik Larson <erik@statropy.com>
2025-02-02 14:11:53 -0500

    Complete assignment 7-2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,29 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+
+LDD_VERSION = d531ca8a75518a5d9403f4195cd9c7e76fa454d0
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-statropy.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
